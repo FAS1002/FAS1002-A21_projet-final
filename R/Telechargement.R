580c0b92ca6c8e1d163b828ea389d86eb1a451ce
EmmaPep <emmanuelle.pepin@umontreal.ca>
2021-12-22 09:19:33 -0500

    Création d'un script pour le téléchargement conditionnel des données

---
+++ R/Telechargement.R
@@ -0,0 +1,9 @@
+# FAS1002 - Projet Final
+## Script de téléchargement des données initiales: RAW
+
+### 1 Our World in Data (OWID)
+### 1.1 Données de vaccination covid-19
+#### Créer une série de commandes pour télécharger le fichier à nouveau
+#### à partir d'OWID si le fichier date de plus d'une journée
+
+
